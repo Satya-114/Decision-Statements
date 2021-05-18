a5ecbda6d66bfe27e8d975c0766c787e0f342e39
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 21:39:52 +0530

    Add files via upload

---
+++ if/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Simple_if_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (5 < 10)
+            {
+                Console.WriteLine("5 is less than 10");
+            }
+        }
+    }
+}
